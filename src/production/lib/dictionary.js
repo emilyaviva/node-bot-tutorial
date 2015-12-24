8f11fcf68454f294c4aa7c9ab229cd5078045589
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2015-12-23 19:47:44 -0800

    added more words

--- src/production/lib/dictionary.js
+++ src/production/lib/dictionary.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var dictionary = {
-  descriptions: ['burnt', 'toasted', 'sizzling', 'sprouted', 'gentrified', 'gourmet', 'hipster', 'bourgeois', 'heirloom', 'organic', 'free-range', 'open-source', 'pastured', 'homeschooled', 'braised', 'pan-seared', 'raw', 'mottled', 'limp', 'warm', 'cave-aged', 'bourbon-barreled', 'malted', 'foraged', 'toothsome', 'deconstructed', 'market', 'intense', 'chocolated'],
+  descriptions: ['burnt', 'toasted', 'sizzling', 'sprouted', 'gentrified', 'gourmet', 'hipster', 'bourgeois', 'heirloom', 'organic', 'free-range', 'open-source', 'pastured', 'homeschooled', 'braised', 'pan-seared', 'raw', 'mottled', 'limp', 'warm', 'cave-aged', 'bourbon-barreled', 'malted', 'foraged', 'toothsome', 'deconstructed', 'market', 'intense', 'chocolated', 'browned', 'salted', 'ambrosial', 'caramelized', 'choice', 'epicurean', 'dulcified', 'healthy', 'kosher', 'halal', 'zesty'],
   ingredients: ['quinoa', 'chanterelle', 'oyster', 'leek', 'algae', 'quail\'s-egg', 'alfalfa', 'cheval', 'cheese', 'grana', 'saffron', 'spelt', 'daikon', 'tamarind', 'lobster', 'geoduck', 'veal', 'swine', 'prosciutto', 'shiso', 'wasabi', 'espresso', 'hochija', 'vanilla', 'balsamico'],
-  foods: ['sandwiches', 'wraps', 'pies', 'bowls', 'burgers', 'plates', 'burritos', 'pods', 'crepes', 'pancakes', 'rolls', 'sushi', 'roulade', 'foam', 'soups'],
-  truckTypes: ['truck', 'cart', 'bike-cart', 'van', 'chariot', 'pedicab', 'vehicle']
+  foods: ['sandwiches', 'wraps', 'pies', 'bowls', 'burgers', 'plates', 'burritos', 'pods', 'crepes', 'pancakes', 'rolls', 'sushi', 'roulade', 'foam', 'soups', 'cakes', 'cookies', 'smoothies', 'juices'],
+  truckTypes: ['truck', 'cart', 'bike-cart', 'van', 'chariot', 'pedicab', 'vehicle', 'conveyance', 'buggy']
 };
 
 module.exports = dictionary;
